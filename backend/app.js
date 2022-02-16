ba3cc453eadce56088f98b7b313f71f3565ff0a1
Josimar-Victoria <josimarvictoria968@gmail.com>
2022-02-16 16:00:03 -0500

    codificando laq opcion de poder crear un post

--- backend/app.js
+++ backend/app.js
@@ -5,4 +5,10 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config({ path: 'backend/config/config.env' })
 }
 
+// Importacion de Routas
+const post = require('./routes/postRouter')
+
+// Uso de rutas
+app.use('/api/v1', post)
+
 module.exports = app
