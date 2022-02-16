80428459d46a95deb1e2bdfcd3dfefed9ae964db
Josimar-Victoria <josimarvictoria968@gmail.com>
2022-02-16 14:22:09 -0500

    creando los models de User

---
+++ backend/models/UserModels.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Por favor ingresa un nombre']
+  },
+
+  avatar: {
+    public_id: String,
+    url: String
+  },
+
+  email: {
+    type: String,
+    required: [true, 'Por favor ingresa un corre eletronico'],
+    unique: [true, 'Este correo eletronico ya existe']
+  },
+  password: {
+    type: String,
+    required: [true, 'Por favor ingresa tu password'],
+    minlength: [6, 'La contraseña debe tener al menos 6 caracteres'],
+    select: false
+  },
+  create_at: {
+    type: Date,
+    default: Date.now
+  },
+  update_at: {
+    type: Date,
+    default: Date.now
+  },
+  verify: {
+    type: Boolean,
+    default: false
+  },
+  enable: {
+    type: Boolean,
+    default: true
+  },
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Post'
+    }
+  ],
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ],
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ]
+})
+
+module.exports = mongoose.model('User', userSchema)
