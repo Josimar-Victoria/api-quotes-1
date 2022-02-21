853c560bc31cb9919ebb112a8cf3b263023588c5
Josimar-Victoria <josimarvictoria968@gmail.com>
2022-02-21 12:14:09 -0500

    user login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -31,3 +31,44 @@ exports.register = async (req, res) => {
     })
   }
 }
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email }).select('+password')
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: 'Este usuario no existe'
+      })
+    }
+
+    const isMatch = await user.matchPassword(password)
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: 'Tu contraseña es incorrecta',
+        success: false
+      })
+    }
+
+    const token = await user.generateToken()
+
+    const options = {
+      expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+      httpOnly: true
+    }
+
+    res
+      .status(201)
+      .cookie('token', token)
+      .json({ success: true, user, token })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    })
+  }
+}
